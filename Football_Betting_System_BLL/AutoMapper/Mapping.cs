63c218b05700f4fb940381fedc0e0b19dc9dd223
Tkach Yevhenii <109665913+TkachYevhenii2303@users.noreply.github.com>
2023-05-04 10:17:20 +0300

    Structure of nested folders inside DAL and BLL + DTO

---
+++ Football_Betting_System_BLL/AutoMapper/Mapping.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football_Betting_System_BLL.AutoMapper
+{
+    internal class Mapping
+    {
+    }
+}
